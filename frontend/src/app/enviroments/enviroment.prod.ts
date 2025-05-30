1e38b13e61d265481a7ede6f3cd3f377c685d2dd
SteveSant26 <Bryanmenoscal2005@gmail.com>
2025-05-29 21:52:35 -0500

    .

--- frontend/src/app/enviroments/enviroment.prod.ts
+++ frontend/src/app/enviroments/enviroment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://apifruteria-hjcnagdcegceckcv.canadacentral-01.azurewebsites.net/docs'
+  // apiUrl: 'https://apifruteria-hjcnagdcegceckcv.canadacentral-01.azurewebsites.net/docs'
+  apiUrl: 'https://modeladoagilsoftwaree-1094652720432.europe-west1.run.app'
 };
